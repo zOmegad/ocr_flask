4abeea06aaeb36cd39d2758827a72abdb80a3ac9
zOmegad <omegad@protonmail.com>
2020-07-08 17:44:54 +0200

    model question

--- models.py
+++ models.py
@@ -4,3 +4,15 @@ from .views import app
 
 # Create database connection object
 db = SQLAlchemy(app)
+
+
+class Request(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    question = db.Column(db.String(200), nullable=False)
+    awnser = db.Column(db.Text(1200), nullable=False)
+
+    def __init__(self, question, awnser):
+        self.question = question
+        self.awnser = awnser
+
+db.create_all()
