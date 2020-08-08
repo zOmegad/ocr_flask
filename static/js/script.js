413033289a09f85a34d7da14be1c4765041f87ae
zOmegad <omegad@protonmail.com>
2020-08-08 10:04:59 +0200

    loading ajax

--- static/js/script.js
+++ static/js/script.js
@@ -5,12 +5,18 @@ $(function(){
 			url: '/',
 			data: $('form').serialize(),
 			type: 'POST',
+			beforeSend: function () { // Before we send the request, remove the .hidden class from the spinner and default to inline-block.
+                $('#loader').addClass('imageLoader')
+            },
 			success: function(response){
-				$("body").html(response);;
+				$("body").html(response);
 			},
 			error: function(error){
 				console.log(error);
-			}
+			},
+			complete: function () { // Set our complete callback, adding the .hidden class and hiding the spinner.
+                $('#loader').removeClass('imageLoader')
+            }
 		});
 	});
 });
